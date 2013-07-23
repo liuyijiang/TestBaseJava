d0d676486d4e5dc7ba1524a7cd5ed6836d339420
liuyijiang <liuyijiang3430@qq.com>
2013-07-23 15:42:39 +0800

    dadawda

---
+++ src/org/basejava/reflect/test/Test.java
@@ -0,0 +1,18 @@
+package org.basejava.reflect.test;
+
+public class Test {
+
+	/** http://www.cnblogs.com/rollenholt/archive/2011/09/02/2163758.html
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			Class claze = Class.forName(DemoBean.class.getName());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
---
+++ src/org/basejava/set/TestQueue.java
@@ -0,0 +1,16 @@
+package org.basejava.set;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class TestQueue {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		BlockingQueue b = new LinkedBlockingQueue(); //阻塞队列
+
+	}
+
+}
